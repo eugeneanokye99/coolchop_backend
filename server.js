f23a94541788f1fc1a2a782dd05afdcac98781e0
Eugene Anokye <134564162+eugeneanokye99@users.noreply.github.com>
2025-01-13 13:00:14 -0800

    webhook function

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.use("/api/cart", cartRoutes);
 // Webhook Route for CI/CD
 app.post('/webhook', (req, res) => {
   const payload = req.body;
+  res.status(200).send('Webhook received');
 
   // Check if the event is a GitHub push event
   if (req.headers['x-github-event'] === 'push' && req.headers['x-github-event'] === 'ping') {
