4395d6876581b48abf826d8727fcfedb59833fc8
Eugene Anokye <134564162+eugeneanokye99@users.noreply.github.com>
2025-01-13 12:45:43 -0800

    .

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const socketServer = new Server(server);
 
 // WebSocket events
 socketServer.on("connection", () => {
-  console.log("WebSocket connection successful");
+  console.log("WebSockett connection successful");
 });
 socketServer.on("disconnect", () => {
   console.log("WebSocket connection disconnected");
